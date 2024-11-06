66e4d7eaa73845d3444a405ea6121d7f4b84e5ad
Milan Nikolic <gen2brain@gmail.com>
2024-11-06 05:34:59 +0100

    Support vendor

---
+++ iup/bind_vendor.go
@@ -0,0 +1,16 @@
+//go:build required
+
+package iup
+
+import (
+	_ "github.com/gen2brain/iup-go/iup/external"
+	_ "github.com/gen2brain/iup-go/iup/external/include"
+	_ "github.com/gen2brain/iup-go/iup/external/src"
+	_ "github.com/gen2brain/iup-go/iup/external/src/cocoa"
+	_ "github.com/gen2brain/iup-go/iup/external/src/gtk"
+	_ "github.com/gen2brain/iup-go/iup/external/src/mot"
+	_ "github.com/gen2brain/iup-go/iup/external/src/win"
+	_ "github.com/gen2brain/iup-go/iup/external/src/win/wdl"
+	_ "github.com/gen2brain/iup-go/iup/external/src/win/wdl/dummy"
+	_ "github.com/gen2brain/iup-go/iup/external/srcgl"
+)
