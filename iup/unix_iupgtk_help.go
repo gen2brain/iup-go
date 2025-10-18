8b482dc68d75a3eee32b0101e69ac2ad71db89d7
Milan Nikolic <gen2brain@gmail.com>
2025-10-18 12:28:02 +0200

    bindings: Update bindings

--- iup/unix_iupgtk_help.go
+++ iup/unix_iupgtk_help.go
@@ -1,4 +1,4 @@
-//go:build (aix || dragonfly || freebsd || linux || netbsd || openbsd || solaris || illumos) && !motif
+//go:build ((aix || dragonfly || freebsd || linux || netbsd || openbsd || solaris || illumos) && !motif) || (gtk && !windows)
 
 package iup
 
