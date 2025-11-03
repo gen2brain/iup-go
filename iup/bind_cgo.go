9e0e25517b25feb811182bc48c9abfa3b603a36b
Milan Nikolic <gen2brain@gmail.com>
2025-11-03 16:30:30 +0100

    bindings: Update #cgo

--- iup/bind_cgo.go
+++ iup/bind_cgo.go
@@ -8,28 +8,37 @@ package iup
 #cgo web CFLAGS: -Iexternal/srcweb
 #cgo web CXXFLAGS: -Iexternal/srcweb
 
-#cgo !windows,!darwin,!motif,!gtk2 CFLAGS: -Iexternal/src/gtk -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED
-#cgo !windows,!darwin,!motif,gtk2 CFLAGS: -Iexternal/src/gtk
-#cgo !windows,!darwin,motif CFLAGS: -Iexternal/src/mot -DIUPDBUS_USE_DLOPEN
-#cgo !windows,!darwin,!motif,web CFLAGS: -DIUPWEB_USE_DLOPEN
+#cgo linux LDFLAGS: -ldl
 #cgo !windows,!darwin LDFLAGS: -lm
 
-#cgo !windows,!darwin,!motif,!gtk2,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0 gdk-wayland-3.0 gdk-x11-3.0 x11
+#cgo !windows,!darwin,!motif,!qt,!gtk2 CFLAGS: -Iexternal/src/gtk -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED
+#cgo !windows,!darwin,!motif,!qt,gtk2 CFLAGS: -Iexternal/src/gtk
+
+#cgo !windows,!darwin,!motif,!qt,!gtk2,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0 gdk-wayland-3.0 gdk-x11-3.0
+#cgo !windows,!darwin,!motif,!qt,gtk2,!nopkgconfig pkg-config: gtk+-2.0 gdk-2.0
+#cgo !windows,!darwin,!motif,!qt,web CFLAGS: -DIUPWEB_USE_DLOPEN
+
 #cgo !windows,!darwin,!motif,!gtk2,gl,!nopkgconfig pkg-config: wayland-egl egl
-#cgo !windows,!darwin,!motif,gtk2,!nopkgconfig pkg-config: gtk+-2.0 gdk-2.0 x11
-#cgo !windows,!darwin,!motif,gtk2,gl,!nopkgconfig pkg-config: gl
+#cgo !windows,!darwin,!motif,!qt,gtk2,gl,!nopkgconfig pkg-config: gl
+
+#cgo qt,!qt5,!nopkgconfig pkg-config: Qt6Core Qt6Gui Qt6Widgets
+#cgo qt,qt5,!nopkgconfig pkg-config: Qt5Core Qt5Gui Qt5Widgets
+#cgo qt,!qt5,web,!nopkgconfig pkg-config: Qt6WebEngineCore Qt6WebEngineWidgets
+#cgo qt,qt5,web,!nopkgconfig pkg-config: Qt5WebEngineCore  Qt5WebEngineWidgets
 
 #cgo !windows,!darwin,motif LDFLAGS: -lXm -lXmu -lXt -lXext -lX11
 #cgo !windows,!darwin,linux,motif LDFLAGS: -lXpm
 #cgo !windows,!darwin,motif,gl LDFLAGS: -lGL
+#cgo !windows,!darwin,motif CFLAGS: -Iexternal/src/mot -DIUPDBUS_USE_DLOPEN
 
 #cgo windows CFLAGS: -Iexternal/src/win -Iexternal/src/win/wdl -DUSE_NEW_DRAW
 #cgo windows CFLAGS: -D_WIN32_WINNT=0x0601 -DWINVER=0x0601 -DCOBJMACROS -DNOTREEVIEW -DUNICODE -D_UNICODE
+#cgo windows LDFLAGS: -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
+#cgo windows,gl LDFLAGS: -lopengl32
 
 #cgo windows,gtk CFLAGS: -Iexternal/src/gtk -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED
 #cgo windows,gtk,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0
-#cgo windows LDFLAGS: -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
-#cgo windows,gl LDFLAGS: -lopengl32
+#cgo windows,gtk,web CFLAGS: -DIUPWEB_USE_DLOPEN
 
 #cgo darwin,!gtk CFLAGS: -Iexternal/src/cocoa -x objective-c
 #cgo darwin,!gtk LDFLAGS: -framework SystemConfiguration -framework QuartzCore -framework Cocoa
