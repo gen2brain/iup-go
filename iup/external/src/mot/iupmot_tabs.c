96437a2040aee9fa5e27629b5cc15b2e2acf1779
Milan Nikolic <gen2brain@gmail.com>
2025-12-05 15:57:35 +0100

    mot: Support SHOWCLOSE in IupTabs

--- iup/external/src/mot/iupmot_tabs.c
+++ iup/external/src/mot/iupmot_tabs.c
@@ -9,6 +9,7 @@
 #include <Xm/BulletinB.h>
 #include <Xm/PushB.h>
 #include <Xm/Label.h>
+#include <Xm/Form.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -34,6 +35,48 @@
 
 #include "iupmot_drv.h"
 #include "iupmot_color.h"
+
+
+#define ITABS_CLOSE_SIZE 12
+
+static Pixmap mot_tabs_close_pixmap = 0;
+
+static void motTabsInitializeClosePixmap(Widget parent)
+{
+  Pixel fg, bg;
+
+  if (mot_tabs_close_pixmap != 0)
+    return;
+
+  static unsigned char close_bits[] = {
+    0x00, 0x00,
+    0x00, 0x00,
+    0x0c, 0x03,
+    0x9c, 0x03,
+    0xf8, 0x01,
+    0xf0, 0x00,
+    0xf0, 0x00,
+    0xf8, 0x01,
+    0x9c, 0x03,
+    0x0c, 0x03,
+    0x00, 0x00,
+    0x00, 0x00
+  };
+
+  Display* dpy = XtDisplay(parent);
+  Window root = RootWindowOfScreen(XtScreen(parent));
+
+  XtVaGetValues(parent, XmNforeground, &fg, XmNbackground, &bg, NULL);
+
+  mot_tabs_close_pixmap = XCreatePixmapFromBitmapData(
+    dpy, root,
+    (char*)close_bits,
+    ITABS_CLOSE_SIZE, ITABS_CLOSE_SIZE,
+    fg,
+    bg,
+    DefaultDepthOfScreen(XtScreen(parent))
+  );
+}
 
 
 int iupdrvTabsExtraMargin(void)
@@ -130,9 +173,18 @@ static void motTabsUpdateButtonsFgColor(Ihandle* ih, Pixel color)
   Ihandle* child;
   for (child = ih->firstchild; child; child = child->brother)
   {
+    Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
     Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-    if (tab_button)
+
+    if (tab_label)
+    {
+      XtVaSetValues(tab_label, XmNforeground, color, NULL);
       XtVaSetValues(tab_button, XmNforeground, color, NULL);
+    }
+    else if (tab_button)
+    {
+      XtVaSetValues(tab_button, XmNforeground, color, NULL);
+    }
   }
 }
 
@@ -141,9 +193,18 @@ static void motTabsUpdateButtonsBgColor(Ihandle* ih, Pixel color)
   Ihandle* child;
   for (child = ih->firstchild; child; child = child->brother)
   {
+    Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
     Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-    if (tab_button)
+
+    if (tab_label)
+    {
+      iupmotSetBgColor(tab_label, color);
       iupmotSetBgColor(tab_button, color);
+    }
+    else if (tab_button)
+    {
+      iupmotSetBgColor(tab_button, color);
+    }
   }
 }
 
@@ -152,10 +213,13 @@ static void motTabsUpdateButtonsPadding(Ihandle* ih)
   Ihandle* child;
   for (child = ih->firstchild; child; child = child->brother)
   {
+    Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
     Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-    if (tab_button)
-      XtVaSetValues(tab_button, XmNmarginHeight, ih->data->vert_padding,
-                                XmNmarginWidth, ih->data->horiz_padding, NULL);
+    Widget target = tab_label ? tab_label : tab_button;
+
+    if (target)
+      XtVaSetValues(target, XmNmarginHeight, ih->data->vert_padding,
+                            XmNmarginWidth, ih->data->horiz_padding, NULL);
   }
 }
 
@@ -166,9 +230,12 @@ static void motTabsUpdatePageFont(Ihandle* ih)
 
   for (child = ih->firstchild; child; child = child->brother)
   {
+    Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
     Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-    if (tab_button)
-      XtVaSetValues(tab_button, XmNrenderTable, fontlist, NULL);
+    Widget target = tab_label ? tab_label : tab_button;
+
+    if (target)
+      XtVaSetValues(target, XmNrenderTable, fontlist, NULL);
   }
 }
 
@@ -297,8 +364,12 @@ static int motTabsSetTabTitleAttrib(Ihandle* ih, int pos, const char* value)
 
   if (value)
   {
+    Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
     Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-    if (tab_button)
+
+    if (tab_label)
+      iupmotSetMnemonicTitle(ih, tab_label, pos, value);
+    else if (tab_button)
       iupmotSetMnemonicTitle(ih, tab_button, pos, value);
   }
 
@@ -307,22 +378,25 @@ static int motTabsSetTabTitleAttrib(Ihandle* ih, int pos, const char* value)
 
 static int motTabsSetTabImageAttrib(Ihandle* ih, int pos, const char* value)
 {
-  Widget tab_button;
+  Widget target;
   Ihandle* child = IupGetChild(ih, pos);
   if (child)
     iupAttribSetStr(child, "TABIMAGE", value);
 
-  tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
-  if (tab_button)
+  Widget tab_label = (Widget)iupAttribGet(child, "_IUPMOT_TABLABEL");
+  Widget tab_button = (Widget)iupAttribGet(child, "_IUPMOT_TABBUTTON");
+  target = tab_label ? tab_label : tab_button;
+
+  if (target)
   {
     if (value)
     {
       Pixmap pixmap = (Pixmap)iupImageGetImage(value, ih, 0, NULL);
       if (pixmap)
-        XtVaSetValues(tab_button, XmNlabelPixmap, pixmap, NULL);
+        XtVaSetValues(target, XmNlabelPixmap, pixmap, NULL);
     }
     else
-      XtVaSetValues(tab_button, XmNlabelPixmap, NULL, NULL);
+      XtVaSetValues(target, XmNlabelPixmap, NULL, NULL);
   }
   return 1;
 }
@@ -400,14 +474,62 @@ static void motTabsPageChangedCallback(Widget w, Ihandle* ih, XmNotebookCallback
 static void motTabsButtonPressEvent(Widget w, Ihandle* child, XButtonEvent* evt, Boolean* cont)
 {
   Ihandle* ih = IupGetParent(child);
-  IFni cb = (IFni)IupGetCallback(ih, "RIGHTCLICK_CB");
+  int pos = iupAttribGetInt(child, "_IUPMOT_TABNUMBER");
   (void)w;
   (void)cont;
 
-  if (evt->type==ButtonPress && evt->button==Button3 && cb)  /* right button clicked on tab */
+  if (evt->type==ButtonPress && evt->button==Button1)
   {
-    int pos = iupAttribGetInt(child, "_IUPMOT_TABNUMBER");
-    cb(ih, pos);
+    Widget tab_close = (Widget)iupAttribGet(child, "_IUPMOT_TABCLOSE");
+    if (tab_close)
+    {
+      Window child_window;
+      int x_rel, y_rel;
+      XTranslateCoordinates(iupmot_display, evt->window, XtWindow(tab_close),
+                            evt->x, evt->y, &x_rel, &y_rel, &child_window);
+
+      Dimension width, height;
+      XtVaGetValues(tab_close, XmNwidth, &width, XmNheight, &height, NULL);
+      if (child_window == XtWindow(tab_close) ||
+          (x_rel >= 0 && x_rel < width && y_rel >= 0 && y_rel < height))
+      {
+        return;
+      }
+
+      iupdrvTabsSetCurrentTab(ih, pos);
+    }
+  }
+  else if (evt->type==ButtonPress && evt->button==Button3)
+  {
+    IFni cb = (IFni)IupGetCallback(ih, "RIGHTCLICK_CB");
+    if (cb)
+      cb(ih, pos);
+  }
+}
+
+static void motTabsCloseButtonActivate(Widget w, XtPointer client_data, XtPointer call_data)
+{
+  Ihandle* child = (Ihandle*)client_data;
+  Ihandle* ih = IupGetParent(child);
+  int pos = iupAttribGetInt(child, "_IUPMOT_TABNUMBER");
+  IFni cb;
+  int ret = IUP_DEFAULT;
+
+  (void)w;
+  (void)call_data;
+
+  cb = (IFni)IupGetCallback(ih, "TABCLOSE_CB");
+  if (cb)
+    ret = cb(ih, pos);
+
+  if (ret == IUP_CONTINUE)
+  {
+    IupDestroy(child);
+    IupRefreshChildren(ih);
+  }
+  else if (ret == IUP_DEFAULT)
+  {
+    IupSetAttributeId(ih, "TABVISIBLE", pos, "NO");
   }
 }
 
@@ -496,38 +618,121 @@ static void motTabsChildAddedMethod(Ihandle* ih, Ihandle* child)
       tabtitle = "     ";
 
     /* Create tabs */
-    /* Label */
-    iupMOT_SETARG(args, num_args, XmNlabelType, tabtitle? XmSTRING: XmPIXMAP);
-    iupMOT_SETARG(args, num_args, XmNmarginHeight, 0);
-    iupMOT_SETARG(args, num_args, XmNmarginWidth, 0);
-    /* Notebook Constraint */
-    iupMOT_SETARG(args, num_args, XmNnotebookChildType, XmMAJOR_TAB);
-    iupMOT_SETARG(args, num_args, XmNpageNumber, pos);
-    tab_button = XtCreateManagedWidget("tab_button", xmPushButtonWidgetClass, ih->handle, args, num_args);
-
-    /* Disable Drag Source */
-    iupmotDisableDragSource(tab_button);
-
-    XtAddEventHandler(tab_button, EnterWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
-    XtAddEventHandler(tab_button, LeaveWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
-    XtAddEventHandler(tab_button, FocusChangeMask, False, (XtEventHandler)iupmotFocusChangeEvent, (XtPointer)ih);
-    XtAddEventHandler(tab_button, KeyPressMask,    False, (XtEventHandler)iupmotKeyPressEvent, (XtPointer)ih);
-
-    XtAddEventHandler(tab_button, ButtonPressMask, False, (XtEventHandler)motTabsButtonPressEvent, (XtPointer)child);
-
-    if (iupStrBoolean(IupGetGlobal("INPUTCALLBACKS")))
+    if (ih->data->show_close)
     {
-      XtAddEventHandler(tab_button, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
-      XtAddEventHandler(child_manager, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
-    }
+      Widget tab_form;
+      Widget tab_label_widget;
+      Widget close_button;
 
-    if (tabtitle)
-      iupmotSetMnemonicTitle(ih, tab_button, pos, tabtitle);
+      num_args = 0;
+      iupMOT_SETARG(args, num_args, XmNmarginHeight, 0);
+      iupMOT_SETARG(args, num_args, XmNmarginWidth, 0);
+      iupMOT_SETARG(args, num_args, XmNborderWidth, 0);
+      iupMOT_SETARG(args, num_args, XmNshadowThickness, 0);
+      iupMOT_SETARG(args, num_args, XmNnotebookChildType, XmMAJOR_TAB);
+      iupMOT_SETARG(args, num_args, XmNpageNumber, pos);
+      tab_form = XtCreateManagedWidget("tab_form", xmFormWidgetClass, ih->handle, args, num_args);
+
+      tab_label_widget = XtVaCreateManagedWidget(
+        "tab_label",
+        xmLabelWidgetClass,
+        tab_form,
+        XmNlabelType, tabtitle ? XmSTRING : XmPIXMAP,
+        XmNmarginHeight, ih->data->vert_padding,
+        XmNmarginWidth, ih->data->horiz_padding,
+        XmNtopAttachment, XmATTACH_FORM,
+        XmNbottomAttachment, XmATTACH_FORM,
+        XmNleftAttachment, XmATTACH_FORM,
+        NULL
+      );
+
+      motTabsInitializeClosePixmap(ih->handle);
+      close_button = XtVaCreateManagedWidget(
+        "close_btn",
+        xmPushButtonWidgetClass,
+        tab_form,
+        XmNlabelType, XmPIXMAP,
+        XmNlabelPixmap, mot_tabs_close_pixmap,
+        XmNmarginHeight, 0,
+        XmNmarginWidth, 0,
+        XmNwidth, ITABS_CLOSE_SIZE + 6,
+        XmNheight, ITABS_CLOSE_SIZE + 6,
+        XmNshadowThickness, 1,
+        XmNhighlightThickness, 0,
+        XmNrecomputeSize, False,
+        XmNtopAttachment, XmATTACH_FORM,
+        XmNbottomAttachment, XmATTACH_FORM,
+        XmNrightAttachment, XmATTACH_FORM,
+        XmNleftAttachment, XmATTACH_WIDGET,
+        XmNleftWidget, tab_label_widget,
+        XmNleftOffset, 4,
+        NULL
+      );
+
+      iupmotDisableDragSource(close_button);
+      XtAddCallback(close_button, XmNactivateCallback,
+                    (XtCallbackProc)motTabsCloseButtonActivate, (XtPointer)child);
+
+      tab_button = tab_form;
+
+      XtAddEventHandler(tab_form, EnterWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_form, LeaveWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_form, FocusChangeMask, False, (XtEventHandler)iupmotFocusChangeEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_form, KeyPressMask,    False, (XtEventHandler)iupmotKeyPressEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_form, ButtonPressMask, False, (XtEventHandler)motTabsButtonPressEvent, (XtPointer)child);
+
+      XtAddEventHandler(tab_label_widget, ButtonPressMask, False, (XtEventHandler)motTabsButtonPressEvent, (XtPointer)child);
+
+      if (iupStrBoolean(IupGetGlobal("INPUTCALLBACKS")))
+      {
+        XtAddEventHandler(tab_form, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
+        XtAddEventHandler(child_manager, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
+      }
+
+      if (tabtitle)
+        iupmotSetMnemonicTitle(ih, tab_label_widget, pos, tabtitle);
+      else
+      {
+        Pixmap pixmap = (Pixmap)iupImageGetImage(tabimage, ih, 0, NULL);
+        if (pixmap)
+          XtVaSetValues(tab_label_widget, XmNlabelPixmap, pixmap, NULL);
+      }
+
+      iupAttribSet(child, "_IUPMOT_TABLABEL", (char*)tab_label_widget);
+      iupAttribSet(child, "_IUPMOT_TABCLOSE", (char*)close_button);
+    }
     else
     {
-      Pixmap pixmap = (Pixmap)iupImageGetImage(tabimage, ih, 0, NULL);
-      if (pixmap)
-        XtVaSetValues(tab_button, XmNlabelPixmap, pixmap, NULL);
+      num_args = 0;
+      iupMOT_SETARG(args, num_args, XmNlabelType, tabtitle? XmSTRING: XmPIXMAP);
+      iupMOT_SETARG(args, num_args, XmNmarginHeight, ih->data->vert_padding);
+      iupMOT_SETARG(args, num_args, XmNmarginWidth, ih->data->horiz_padding);
+      iupMOT_SETARG(args, num_args, XmNnotebookChildType, XmMAJOR_TAB);
+      iupMOT_SETARG(args, num_args, XmNpageNumber, pos);
+      tab_button = XtCreateManagedWidget("tab_button", xmPushButtonWidgetClass, ih->handle, args, num_args);
+
+      iupmotDisableDragSource(tab_button);
+
+      XtAddEventHandler(tab_button, EnterWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_button, LeaveWindowMask, False, (XtEventHandler)iupmotEnterLeaveWindowEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_button, FocusChangeMask, False, (XtEventHandler)iupmotFocusChangeEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_button, KeyPressMask,    False, (XtEventHandler)iupmotKeyPressEvent, (XtPointer)ih);
+      XtAddEventHandler(tab_button, ButtonPressMask, False, (XtEventHandler)motTabsButtonPressEvent, (XtPointer)child);
+
+      if (iupStrBoolean(IupGetGlobal("INPUTCALLBACKS")))
+      {
+        XtAddEventHandler(tab_button, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
+        XtAddEventHandler(child_manager, PointerMotionMask, False, (XtEventHandler)iupmotDummyPointerMotionEvent, NULL);
+      }
+
+      if (tabtitle)
+        iupmotSetMnemonicTitle(ih, tab_button, pos, tabtitle);
+      else
+      {
+        Pixmap pixmap = (Pixmap)iupImageGetImage(tabimage, ih, 0, NULL);
+        if (pixmap)
+          XtVaSetValues(tab_button, XmNlabelPixmap, pixmap, NULL);
+      }
     }
 
     background = iupBaseNativeParentGetBgColorAttrib(ih);
@@ -580,11 +785,12 @@ static void motTabsChildRemovedMethod(Ihandle* ih, Ihandle* child, int pos)
       XtDestroyWidget(tab_button);
       XtDestroyWidget(child_manager);
 
-      /* compact the tab number usage */
       motTabsUpdatePageNumber(ih);
 
       iupAttribSet(child, "_IUPTAB_CONTAINER", NULL);
       iupAttribSet(child, "_IUPMOT_TABBUTTON", NULL);
+      iupAttribSet(child, "_IUPMOT_TABLABEL", NULL);
+      iupAttribSet(child, "_IUPMOT_TABCLOSE", NULL);
       iupAttribSet(child, "_IUPMOT_TABNUMBER", NULL);
 
       iupAttribSet(ih, "_IUPMOT_IGNORE_PAGECHANGE", NULL);
@@ -680,6 +886,9 @@ void iupdrvTabsInitClass(Iclass* ic)
     XtActionsRec rec = {"iupTabsConfigure", motTabsConfigureNotify};
     XtAppAddActions(iupmot_appcontext, &rec, 1);
   }
+
+  /* Register TABCLOSE_CB callback */
+  iupClassRegisterCallback(ic, "TABCLOSE_CB", "i");
 
   /* Driver Dependent Attribute functions */
 
