a2517abd5e095ab9a2d0e23c2064cc647179c431
Milan Nikolic <gen2brain@gmail.com>
2026-03-14 13:07:19 +0100

    bindings: Update cgo

--- iup/bind_cgo.go
+++ iup/bind_cgo.go
@@ -14,7 +14,9 @@ package iup
 #cgo linux LDFLAGS: -ldl
 #cgo !windows,!darwin LDFLAGS: -lm
 
-#cgo !windows,!darwin,!qt,!efl CFLAGS: -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED -DIUPDBUS_USE_DLOPEN
+#cgo !windows,!darwin CFLAGS: -DIUPDBUS_USE_DLOPEN
+
+#cgo !windows,!darwin,!qt,!efl,!motif CFLAGS: -Iexternal/src/unix -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED
 #cgo gtk,gtk2,gtk4 CFLAGS: -DGDK_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED
 
 #cgo !windows,!darwin,!motif,!qt,!gtk2,!gtk4,!efl CFLAGS: -Iexternal/src/gtk -DIUP_USE_GTK3
@@ -23,7 +25,6 @@ package iup
 
 #cgo qt CFLAGS: -Iexternal/src/qt -DIUP_USE_QT
 #cgo qt CXXFLAGS: -Iexternal/src/qt -DIUP_USE_QT
-#cgo qt,!windows,!darwin CFLAGS: -DIUPDBUS_USE_DLOPEN
 
 #cgo !windows,!darwin,!motif,!qt,!gtk2,!gtk4,!efl,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0 gdk-wayland-3.0 gdk-x11-3.0
 #cgo !windows,!darwin,!motif,!qt,!gtk2,gtk4,!efl,!nopkgconfig pkg-config: gtk4 gtk4-wayland gtk4-x11
@@ -44,14 +45,14 @@ package iup
 #cgo motif,gl LDFLAGS: -lGL
 #cgo motif,xft CFLAGS: -DIUP_USE_XFT
 #cgo motif,xft,!nopkgconfig pkg-config: xft freetype2
-#cgo motif CFLAGS: -Iexternal/src/mot -DIUP_USE_ICONV
+#cgo motif CFLAGS: -Iexternal/src/mot -Iexternal/src/unix -DIUP_USE_ICONV
 
 #cgo windows CFLAGS: -Iexternal/src/win -Iexternal/src/win/wdl
 #cgo windows,!gtk,!gtk4,!qt,!winui CFLAGS: -D_WIN32_WINNT=0x0601 -DWINVER=0x0601 -DCOBJMACROS -DNOTREEVIEW -DUNICODE -D_UNICODE
 #cgo windows LDFLAGS: -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
 #cgo windows,gl LDFLAGS: -lopengl32
 
-#cgo windows,gtk CFLAGS: -Iexternal/src/gtk -DIUP_USE_GTK3
+#cgo windows,gtk CFLAGS: -Iexternal/src/gtk -Iexternal/src/unix -DIUP_USE_GTK3
 #cgo windows,gtk,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0
 
 #cgo windows,gtk4 CFLAGS: -Iexternal/src/gtk4 -DIUP_USE_GTK4
@@ -59,14 +60,14 @@ package iup
 
 #cgo winui CFLAGS: -Iexternal/src/winui -DIUP_USE_WINUI -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000000
 #cgo winui CXXFLAGS: -Iexternal/src/winui -DIUP_USE_WINUI -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000000 -DUNICODE -D_UNICODE -std=c++20 -stdlib=libc++
-#cgo winui LDFLAGS: -lwindowsapp -lruntimeobject -ld2d1 -ld3d11 -ldxgi -ldwrite -lole32 -loleaut32 -luuid -luser32 -lshell32 -l:libc++.a -l:libc++abi.a
+#cgo winui LDFLAGS: -lwindowsapp -lruntimeobject -ld2d1 -ldwrite -luuid -loleaut32 -lole32 -luser32 -lshell32 -l:libc++.a -l:libc++abi.a
 
 #cgo darwin CFLAGS: -Iexternal/src/cocoa -x objective-c
 #cgo darwin LDFLAGS: -framework SystemConfiguration -framework QuartzCore -framework AppKit -framework UserNotifications
 #cgo darwin,gl LDFLAGS: -framework OpenGL
 #cgo darwin,!gtk,!gtk4,!qt,web LDFLAGS: -framework WebKit
 
-#cgo darwin,gtk CFLAGS: -Iexternal/src/gtk -DIUP_USE_GTK3 -x objective-c
+#cgo darwin,gtk CFLAGS: -Iexternal/src/gtk -Iexternal/src/unix -DIUP_USE_GTK3 -x objective-c
 #cgo darwin,gtk,!nopkgconfig pkg-config: gtk+-3.0 gdk-3.0
 #cgo darwin,gtk,web CFLAGS: -DIUPWEB_USE_DLOPEN
 
@@ -74,8 +75,8 @@ package iup
 #cgo darwin,gtk4,!nopkgconfig pkg-config: gtk4
 #cgo darwin,gtk4,web CFLAGS: -DIUPWEB_USE_DLOPEN
 
-#cgo efl CFLAGS: -Iexternal/src/efl -DIUP_USE_EFL -DEFL_BETA_API_SUPPORT=1 -DEFL_EO_API_SUPPORT=1 -DHAVE_ECORE_X -DHAVE_ECORE_WL2
-#cgo efl,!nopkgconfig pkg-config: elementary ecore-x ecore-wl2 dbus-1
+#cgo efl CFLAGS: -Iexternal/src/efl -Iexternal/src/unix -DIUP_USE_EFL -DEFL_BETA_API_SUPPORT=1 -DEFL_EO_API_SUPPORT=1 -DHAVE_ECORE_X -DHAVE_ECORE_WL2
+#cgo efl,!nopkgconfig pkg-config: elementary ecore-x ecore-wl2
 #cgo efl,gl,!nopkgconfig pkg-config: wayland-egl egl gl
 */
 import "C"
