3e5bae94bb55d73868ac7deefba439d5aa4cc177
Milan Nikolic <gen2brain@gmail.com>
2026-04-03 00:40:17 +0200

    examples: Consolidate menu examples

--- examples/menu/menu.go
+++ examples/menu/menu.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gen2brain/iup-go/iup"
 )
 
@@ -8,25 +10,68 @@ func main() {
 	iup.Open()
 	defer iup.Close()
 
-	itemOpen := iup.MenuItem("Open").SetAttribute("KEY", "O")
-	itemSave := iup.MenuItem("Save").SetAttribute("KEY", "S")
-	itemUndo := iup.MenuItem("Undo").SetAttributes("KEY=U, ACTIVE=NO")
-	itemExit := iup.MenuItem("Exit").SetAttribute("KEY", "x")
+	iup.Menu(
+		iup.Submenu("File",
+			iup.Menu(
+				iup.MenuItem("New").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuItem("Open").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuItem("Close").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuSeparator(),
+				iup.MenuItem("Page Setup").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuItem("Print").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuSeparator(),
+				iup.MenuItem("Exit").SetCallback("ACTION", iup.ActionFunc(itemExitCb)),
+			),
+		),
+		iup.Submenu("Edit",
+			iup.Menu(
+				iup.MenuItem("Copy").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuItem("Paste").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+				iup.MenuSeparator(),
+				iup.Submenu("Create",
+					iup.Menu(
+						iup.MenuItem("Line").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+						iup.MenuItem("Circle").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+						iup.Submenu("Triangle",
+							iup.Menu(
+								iup.MenuItem("Equilateral").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+								iup.MenuItem("Isosceles").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+								iup.MenuItem("Scalene").SetCallback("ACTION", iup.ActionFunc(itemCb)),
+							),
+						),
+					),
+				),
+			),
+		),
+		iup.Submenu("Help",
+			iup.Menu(
+				iup.MenuItem("Help").SetCallback("ACTION", iup.ActionFunc(itemHelpCb)),
+			),
+		),
+	).SetHandle("menu")
 
-	itemExit.SetCallback("ACTION", iup.ActionFunc(func(ih iup.Ihandle) int { return iup.CLOSE }))
-
-	fileMenu := iup.Menu(itemOpen, itemSave, iup.MenuSeparator(), itemUndo, itemExit)
-	subMenu := iup.Submenu("File", fileMenu)
-
-	menu := iup.Menu(subMenu)
-	menu.SetHandle("mymenu")
-
-	dlg := iup.Dialog(iup.Canvas()).SetAttributes(map[string]string{
-		"MENU":  "mymenu",
+	dlg := iup.Dialog(
+		iup.Canvas(),
+	).SetAttributes(map[string]string{
 		"TITLE": "Menu",
 		"SIZE":  "QUARTERxQUARTER",
+		"MENU":  "menu",
 	})
 
-	iup.Show(dlg)
+	iup.ShowXY(dlg, iup.CENTER, iup.CENTER)
 	iup.MainLoop()
+}
+
+func itemCb(ih iup.Ihandle) int {
+	fmt.Printf("Item %q selected\n", ih.GetAttribute("TITLE"))
+	return iup.DEFAULT
+}
+
+func itemHelpCb(ih iup.Ihandle) int {
+	iup.Message("Help", "Only Help and Exit items perform an operation")
+	return iup.DEFAULT
+}
+
+func itemExitCb(ih iup.Ihandle) int {
+	return iup.CLOSE
 }
